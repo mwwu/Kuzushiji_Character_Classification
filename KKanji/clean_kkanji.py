22dd3fa3f56de629e49e80ccbd166cdaf053d39f
mynktwri <mayank@rockinfamily.com>
2019-02-18 05:17:47 -0800

    kkanji image crawling is set up, now just needs to have code to put it into numpy style data

--- KKanji/clean_kkanji.py
+++ KKanji/clean_kkanji.py
@@ -1,8 +1,13 @@
 import os
+import imageio
+import glob
 #base_dir = "\kkanji2\U+4C61\e6eeb23552e1a21c.png"
 base_dir = "kkanji2"
 
 for dirName, subdirList, fileList in os.walk(base_dir):
-    print(dirName[10:]) # labels
-    for fname in fileList:
-         print('\t%s -> %s' % (open(dirName + "\\" +  fname), dirName[10:])) # actual files for data
+    # print(dirName[10:]) # labels
+    for im_path in glob.glob(dirName + "\\*.png"):
+        # print(im_path)
+        im = imageio.imread(im_path)
+        print(im.shape)
+        # TODO: take im and make it into a numpy with dirName[10:] as the label name.
